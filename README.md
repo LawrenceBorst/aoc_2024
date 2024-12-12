6adb2d129fdc78610f21c23402af52ceee7b8e0b
Lawrence Borst <laurens.s.borst@gail.com>
2024-12-12 23:04:48 +0100

    Add problem argument to the main script
    In this commit we dynamically grab the right modules to run the solution on
    The script is now executed with a problem (integer) argument

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Advent of Code 2024 Solutions
 A simple Python project in which we construct the solutions to the advent of code 2024 questions. Good annual practice with testing, Python (or whatever programming language of choice) and algorithms in general.
 
-To run the script use ```poetry install``` followed by ```poetry run main```
+To run the script use ```poetry install``` followed by ```poetry run main [problem]```
--- main.py
+++ main.py
@@ -1,16 +1,48 @@
 from pathlib import Path, PosixPath
-from advent_of_code_2024.solution_03 import Solution03, DataLoader03
+from advent_of_code_2024.utils import Solution, DataLoader
+import importlib
+import argparse
 
 
-def main():
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Gets solutions to the AOC problems")
+    parser.add_argument("problem", type=int, help="the number of the problem")
+
+    args = parser.parse_args()
+
+    if not validate_args(args):
+        raise Exception('Problem argument not in the range 1 to 25 inclusive')
+
+    run(args.problem)
+
+
+def validate_args(args) -> bool:
+    problem: int = args.problem
+
+    if problem <= 0 or problem > 25:
+        return False
+    
+    return True
+
+
+def run(problem: int) -> None:
     base_path: PosixPath = Path(__file__).parent
+
+    sol_str: str = str(problem).zfill(2)
+    class_module = importlib.import_module(f"advent_of_code_2024.solution_{sol_str}")
+
     input_path = (
-        base_path / "advent_of_code_2024" / "solution_03" / "input.txt"
+        base_path / "advent_of_code_2024" / f"solution_{sol_str}" / "input.txt"
     ).resolve()
-    loader: DataLoader03 = DataLoader03(str(input_path))
-    solution: Solution03 = Solution03(loader)
+
+    loader: DataLoader = getattr(class_module, f"DataLoader{sol_str}", None)(
+        str(input_path)
+    )
+
+    solution: Solution = getattr(class_module, f"Solution{sol_str}", None)(loader)
 
     print(solution.solution_1())
+    print(solution.solution_2())
 
 
 if __name__ == "__main__":
